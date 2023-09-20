d47cef889cc637c0dda06c3171f1e6f0dacd60cd
mnmkato <katsmamal@gmail.com>
2023-09-20 23:30:12 +0300

    vit config commit

--- gallery-project/vite.config.js
+++ gallery-project/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://mnmkato.github.io/stage3",
+  base:"https://mnmkato.github.io/stage3/",
   css: {
     modules: false,
   },
