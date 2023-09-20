9a62e6a63441017cd1f0ca92a11b0d54464a6f4e
mnmkato <katsmamal@gmail.com>
2023-09-20 23:19:38 +0300

    commit gh pages config

--- gallery-project/vite.config.js
+++ gallery-project/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://mnmkato.github.io/stage3",
   css: {
     modules: false,
   },
