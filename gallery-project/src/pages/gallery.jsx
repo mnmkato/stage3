019db2b424940d83a9c44829506a81ccd57f0cea
mnmkato <katsmamal@gmail.com>
2023-09-23 14:28:13 +0300

    refined dnd

--- gallery-project/src/pages/gallery.jsx
+++ gallery-project/src/pages/gallery.jsx
@@ -1,55 +1,61 @@
 import '../App.css';
 import pics from '../pics';
 import React, { useState, useEffect } from 'react';
-import auth from "../firebase";
+import auth from '../firebase';
 import { onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import GalleryHeader from '../components/GalleryHeader';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function Gallery() {
+  const [selected, setSelected] = useState(0);
   const [data, setData] = useState({
     pics: pics,
     filteredData: pics,
   });
   const [searchQuery, setSearchQuery] = useState('');
-  const [draggedItemId, setDraggedItemId] = useState(null); 
 
   useEffect(() => {
     const filteredData = data.pics.filter((item) =>
       item.tags.some((tag) => tag.toLowerCase().includes(searchQuery.toLowerCase()))
     );
     setData((prevData) => ({ ...prevData, filteredData }));
+    setSelected(0);
   }, [searchQuery, data.pics]);
 
   const handleSearchQueryChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
-  const onDragStart = (initial) => {
-    setDraggedItemId(initial.draggableId);
+  const [draggingIndex, setDraggingIndex] = useState(null);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData('text/plain', index.toString());
+    setDraggingIndex(index);
   };
 
-  const onDragEnd = (result) => {
-    setDraggedItemId(null);
+  const handleDragOver = (e, index) => {
+    e.preventDefault();
+    setHoveredIndex(index);
+  };
 
-    if (!result.destination) return;
-
-    const sourceIndex = result.source.index;
-    const destinationIndex = result.destination.index;
-
-    console.log(
-      "sourceIndex "+result.source.index+
-      " destinationIndex "+result.destination.index)
-
+  const handleDrop = (e, index) => {
+    e.preventDefault();
+    const sourceIndex = draggingIndex;
     const newData = [...data.filteredData];
     const [draggedItem] = newData.splice(sourceIndex, 1);
-    newData.splice(destinationIndex, 0, draggedItem);
-
+    newData.splice(index, 0, draggedItem);
     setData((prevData) => ({
       ...prevData,
       filteredData: newData,
     }));
+    setDraggingIndex(null);
+    setHoveredIndex(null);
+  };
+
+  const handleDragEnd = () => {
+    setDraggingIndex(null);
+    setHoveredIndex(null);
   };
 
   const navigate = useNavigate();
@@ -63,7 +69,28 @@ function Gallery() {
     });
     return () => unsubscribe();
   }, [navigate]);
-  
+
+  const handleTouchStart = (index) => {
+    setDraggingIndex(index);
+  };
+
+  const handleTouchMove = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleTouchEnd = () => {
+    const sourceIndex = draggingIndex;
+    const newData = [...data.filteredData];
+    const [draggedItem] = newData.splice(sourceIndex, 1);
+    newData.splice(hoveredIndex, 0, draggedItem);
+    setData((prevData) => ({
+      ...prevData,
+      filteredData: newData,
+    }));
+    setDraggingIndex(null);
+    setHoveredIndex(null);
+  };
+
   return (
     <>
       <GalleryHeader searchQuery={searchQuery} handleSearchQueryChange={handleSearchQueryChange} />
@@ -71,51 +98,45 @@ function Gallery() {
         {data.filteredData.length === 0 ? (
           <div className="results-not-found">Results Not Found</div>
         ) : (
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="gallery" direction='horizontal'>
-              {(provided) => (
+          <>
+            <div className="gallery">
+              {data.filteredData.map((item, index) => (
                 <div
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                  className="gallery"
+                  key={item.id}
+                  className={
+                    index === selected
+                      ? 'gallery-item selected-photo'
+                      : index === draggingIndex
+                      ? 'gallery-item dragged-photo'
+                      : index === hoveredIndex
+                      ? 'gallery-item hovered-photo'
+                      : 'gallery-item'
+                  }
+                  onClick={() => setSelected(index)}
+                  draggable="true"
+                  onDragStart={(e) => handleDragStart(e, index)}
+                  onDragOver={(e) => handleDragOver(e, index)}
+                  onDrop={(e) => handleDrop(e, index)}
+                  onDragEnd={handleDragEnd}
+                  onTouchStart={() => handleTouchStart(index)}
+                  onTouchMove={() => handleTouchMove(index)}
+                  onTouchEnd={handleTouchEnd}
                 >
-                  {data.filteredData.map((item, index) => (
-                    <Draggable
-                    key={item.id.toString()}
-                    draggableId={item.id.toString()}
-                    index={index}
-                    isDragDisabled={false}
-                    onStart={() => onDragStart({ draggableId: item.id })}
-                  >
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        className={`gallery-item ${snapshot.isDragging ? 'isDragging' : ''}`}
-                      >
-                        <img src={item.photo} alt="" />
-                        <div className="gallery-item-tags">
-                          {item.tags.map((tag, index) => (
-                            <span key={index} className="tag">
-                              {tag}
-                            </span>
-                          ))}
-                        </div>
-                      </div>
-                    )}
-                  </Draggable>
-                  
-                  ))}
-                  {provided.placeholder}
+                  <img src={item.photo} alt="" />
+                  <div className="gallery-item-tags">
+                    {data.filteredData[index].tags.map((tag, index) => (
+                      <span key={index} className="tag">
+                        {tag}
+                      </span>
+                    ))}
+                  </div>
                 </div>
-              )}
-            </Droppable>
-          </DragDropContext>
+              ))}
+            </div>
+          </>
         )}
       </div>
     </>
   );
 }
-
 export default Gallery;
